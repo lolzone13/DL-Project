a66d3b0d8a68d5ddbe66da46aeec0ef08465a6e7
Mohit Manoj <72098715+lolzone13@users.noreply.github.com>
2023-10-20 23:30:03 +0530

    added keyboard and mouse input

--- fps.py
+++ fps.py
@@ -1,6 +1,6 @@
 from ursina import *
-from random import uniform
-from ursina.prefabs.first_person_controller import FirstPersonController
+# from random import uniform
+# from ursina.prefabs.first_person_controller import FirstPersonController
 
 # def input(key):
 # 	if key=="left mouse down":
@@ -36,37 +36,76 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 # 			collider="box"
 # 			)
 
-app=Ursina()
+# app=Ursina()
 
-Sky()
-player=FirstPersonController(y=2, origin_y=-.5)
-ground=Entity(model='plane', scale=(100, 1, 100), color=color.lime, texture="white_cube",
-	texture_scale=(100, 100), collider='box')
+# Sky()
+# player=FirstPersonController(y=2, origin_y=-.5)
+# ground=Entity(model='plane', scale=(100, 1, 100), color=color.lime, texture="white_cube",
+# 	texture_scale=(100, 100), collider='box')
 
 # wall_1=Entity(model="cube", collider="box", position=(-8, 0, 0), scale=(8, 5, 1), rotation=(0,0,0),
 # 	texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
-# wall_2 = duplicate(wall_1, z=5)
-# wall_3=duplicate(wall_1, z=10)
-# wall_4=Entity(model="cube", collider="box", position=(-15, 0, 10), scale=(1,5,20), rotation=(0,0,0),
+# # wall_2 = duplicate(wall_1, z=20)
+# # wall_3=duplicate(wall_1, z=10)
+# wall_4=Entity(model="cube", collider="box", position=(-15, 0, 10), scale=(1, 5, 1), rotation=(0,0,0),
 # 	texture="brick", texture_scale=(5,5), color=color.rgb(255, 128, 0))
 
-# gun=Entity(model="assets/gun.obj", parent=camera.ui, scale=.08, color=color.gold, position=(.3, -.2),
-# 	rotation=(-5, -10, -10))
+# # gun=Entity(model="assets/gun.obj", parent=camera.ui, scale=.08, color=color.gold, position=(.3, -.2),
+# # 	rotation=(-5, -10, -10))
 
-# num=6
-# wasps=[None]*num
-# spiders=[None]*num
-# for i in range(num):
-# 	wx=uniform(-12, -7)
-# 	wy=uniform(.1, 1.8)
-# 	wz=uniform(.8, 3.8)
-# 	wasps[i]=Wasp(wx, wy, wz)
-# 	wasps[i].animate_x(wx+.5, duration=.2, loop=True)
+# # num=6
+# # wasps=[None]*num
+# # spiders=[None]*num
+# # for i in range(num):
+# # 	wx=uniform(-12, -7)
+# # 	wy=uniform(.1, 1.8)
+# # 	wz=uniform(.8, 3.8)
+# # 	wasps[i]=Wasp(wx, wy, wz)
+# # 	wasps[i].animate_x(wx+.5, duration=.2, loop=True)
 
-# 	sx=uniform(-12, -7)
-# 	sy=uniform(.1, 1.8)
-# 	sz=uniform(5.8, 8.8)
-# 	spiders[i]=Spider(sx, sy, sz)
-# 	spiders[i].animate_x(sx+.5, duration=.2, loop=True)
+# # 	sx=uniform(-12, -7)
+# # 	sy=uniform(.1, 1.8)
+# # 	sz=uniform(5.8, 8.8)
+# # 	spiders[i]=Spider(sx, sy, sz)
+# # 	spiders[i].animate_x(sx+.5, duration=.2, loop=True)
 
+# app.run()
+
+
+from ursina import Ursina, Sky, load_model, color, Text, window
+app = Ursina(vsync=False)
+'''
+Simple camera for debugging.
+Hold right click and move the mouse to rotate around point.
+'''
+
+sky = Sky()
+e = Entity(model=load_model('cube', use_deepcopy=True), color=color.white, collider='box')
+e.model.colorize()
+
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+ground = Entity(model='plane', scale=32, texture='white_cube', texture_scale=(32,32), collider='box')
+box = Entity(model='cube', collider='box', texture='white_cube', scale=(10,2,2), position=(2,1,5), color=color.light_gray)
+player = FirstPersonController(y=1, enabled=True)
+
+ec = EditorCamera()
+ec.enabled = False
+rotation_info = Text(position=window.top_left)
+
+
+
+
+def update():
+    rotation_info.text = str(int(ec.rotation_y)) + '\n' + str(int(ec.rotation_x))
+
+
+def input(key):
+    if key == 'tab':    # press tab to toggle edit/play mode
+        ec.enabled = not ec.enabled
+    elif key == 'space':
+        mouse.locked = True
+        print(mouse.velocity)
 app.run()
+
+
---
+++ keyboard_input.py
@@ -0,0 +1,11 @@
+import keyboard
+import time
+import mouse
+
+time.sleep(5)
+
+keyboard.press("w")
+
+time.sleep(5)
+
+keyboard.release("w")
